2ea93ca3afbf7b9f5ba7c6d5712b217893ceb560
Christian Padilla <hazrdous.51010@gmail.com>
2025-06-16 13:20:43 -0700

    6/16

---
+++ lab2.py
@@ -0,0 +1,16 @@
+# Lab 2: Python Profiles
+
+## Step 1
+name = 'Christian Padilla'
+
+## Step 2
+nickname = 'Chris'
+
+##Step 3
+age = 16.0
+
+##Step 4
+used_python = True
+
+##Step 5
+hobbies = ("Games", "producing music", "using my PSP")
