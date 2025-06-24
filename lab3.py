fbc4204acc9f91970f6315909f1ab84f39151e82
Christian Padilla <hazrdous.51010@gmail.com>
2025-06-24 11:16:23 -0700

    Lab 3 Assignment Completion

--- lab3.py
+++ lab3.py
@@ -1,25 +1,40 @@
 # Lab 3
 
-# Working with Strings
+# 1. Working with Strings
 
 food = 'Gyoza'
-print(food[0:3])
-print(food[-3:])
+#print(food[0:3])
+#print(food[-3:])
 first_last = food[0] + food[-1]
-print(first_last)
+#print(first_last)
 food_list = food.split()
-print(food_list)
+#print(food_list)
 joined_food = ' '.join(food_list)
-print(joined_food)
+#print(joined_food)
 
-# Working with Lists
+# 2. Working with Lists
 number_list = [17, 2, 32, 43, 89]
 number_list.append(21)
-print(number_list)
+#print(number_list)
 number_list.insert(3, 2000)
-print(number_list)
+#print(number_list)
 number_list.pop()
-print(number_list)
+#print(number_list)
 number_list.remove(number_list[1])
+#print(number_list)
+#print(number_list[:3])
+#print(number_list[-3:])
+
+# 3. Working with Dictionarys
+books = {'Suzanne Collins': 'The Hunger Games', 'George Orwell': '1984', 'Rick Riordan': 'Percy Jackson', 'Jeff Kinney': 'Diary of a Wimpy Kid'}
+#print(books.keys())
+#print(books.values())
+#print(books.get('Suzanne Collins'))
+#print(books.pop(list(books.keys())[2]))
+#print(books)
+del books[list(books.keys())[0]]
+print(books)
+
+
 
 
