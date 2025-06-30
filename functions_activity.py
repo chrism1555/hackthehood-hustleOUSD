38c3b4b66a4b7955419349a87dd5433d1c239989
Christian Padilla <hazrdous.51010@gmail.com>
2025-06-29 19:46:10 -0700

    total price

---
+++ functions_activity.py
@@ -0,0 +1,14 @@
+
+
+menu = {'Pizza': 2.99, 'Burger': 3.99, 'Hot dog': 1.99, 'Cheese': 0.59, 'Ice cream': 1.49, 'Churro': 0.79, 'Soda': 0.89}
+# Menu holds the value of each item on the dictionary
+
+
+def total_price(Burger, Cheese):
+
+   total = (Burger + Cheese)
+
+   return total
+
+print(total_price(3.99, 0.59))
+# Prints the sum of both items together
