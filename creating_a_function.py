322cd658aa44790566d311b5723a2978d585dca2
Christian Padilla <hazrdous.51010@gmail.com>
2025-06-29 19:23:13 -0700

    creating a function 6/29

---
+++ creating_a_function.py
@@ -0,0 +1,13 @@
+
+
+def triangle_area(base, height):
+
+   area = (1/2) * (base * height)
+
+   return area
+
+print(triangle_area(3, 4))
+# will return 6 and print to the console
+
+print(triangle_area(8, 11))
+# will return 44 and print to console
