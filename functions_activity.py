b76e07bda328bfdb0041acb419ee2e54798cd2fd
Christian Padilla <hazrdous.51010@gmail.com>
2025-06-29 23:45:32 -0700

    6/29 11:45PM

--- functions_activity.py
+++ functions_activity.py
@@ -1,14 +1,57 @@
 
 
-menu = {'Pizza': 2.99, 'Burger': 3.99, 'Hot dog': 1.99, 'Cheese': 0.59, 'Ice cream': 1.49, 'Churro': 0.79, 'Soda': 0.89}
+menu = {'Pizza': 2.99, 
+        'Burger': 3.99, 
+        'Hotdog': 1.99, 
+        'Cheese': 0.59, 
+        'Ice cream': 1.49,
+        'Churro': 0.79, 
+        'Soda': 0.89}
 # Menu holds the value of each item on the dictionary
 
+# total_price("Pizza","Hotdog")
+def total_price(item1, item2):
 
-def total_price(Burger, Cheese):
-
-   total = (Burger + Cheese)
+   total = menu[item1] + menu[item2]
 
    return total
 
-print(total_price(3.99, 0.59))
-# Prints the sum of both items together
+#print(total_price("Burger","Hotdog"))
+# Prints the sum of both items
+
+
+def price_difference(Pizza, Soda):
+
+   difference = menu[Pizza] - menu[Soda]
+
+   return difference
+
+#print(price_difference("Pizza","Soda"))
+# Prints the value difference of both items
+
+
+def inflation(item, increase):
+
+   menu[item] = menu[item] * increase
+
+   return menu
+
+def deflation(item, divide):
+   
+   menu[item] = menu[item] / divide  
+
+   return menu
+
+#print(deflation("Pizza", 2))
+#print(inflation("Burger", 2))
+
+menu = {'Pizza': 2.99, 
+        'Burger': 3.99, 
+        'Hotdog': 1.99, 
+        'Cheese': 0.59, 
+        'Ice cream': 1.49,
+        'Churro': 0.79, 
+        'Soda': 0.89}
+menu["Popcorn"] = "4.99" # type: ignore
+
+print(menu)
