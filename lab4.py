d3ccdd901cc41d09633dd25adf5cc806e01ad9fd
Christian Padilla <hazrdous.51010@gmail.com>
2025-06-30 12:59:20 -0700

    lab4

---
+++ lab4.py
@@ -0,0 +1,24 @@
+checking = "yes"
+
+while checking == "yes":
+    print("What is your age: ")
+    user_input = input ()
+    if int(user_input) >= 18:
+        print("Yes you can vote")
+    else:
+        print ("You can't vote")
+    print("Would you like to check another age?")
+    user_input2 = input ( )
+    checking = user_input2
+
+list1 = [3, -1, 0, 6, -4]
+
+for x in list1:
+    if x > 0:
+         print("value is positive")
+    elif x < 0:
+         print("Value is negative")
+    else:
+         print("NUmber is zero")
+
+inventory = ["int", "glass"]
