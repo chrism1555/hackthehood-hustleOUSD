2f6b7308ef19bdebcf6387dfc5b8441214d07476
Christian Padilla <hazrdous.51010@gmail.com>
2025-07-06 23:58:22 -0700

    7/6

--- crud_activity.py
+++ crud_activity.py
@@ -2,16 +2,64 @@ cookbook_list = []
 
 def create(recipe):
 
-      return print("A new recipe has been added to the list!")
+   return print("A new recipe has been added to the list!")
 
 def read(index):
-      
-      return index()
+
+   return index
 
 def update(index, recipe):
 
-      return print("The recipe has been updated!")
+   return print("The recipe has been updated!")
 
 def destroy():
       
-      return print("A recipe has been removed!")
+   return print("A recipe has been removed!")
+
+def list_all_recipes():
+      
+   for i in cookbook_list:
+      if i == "recipe":
+         print("ingredient")
+
+
+def main():
+    while True:
+        print("\nCookbook Recipes")
+        print("1. Add a Recipe")
+        print("2. Read a Recipe")
+        print("3. Update a Recipe")
+        print("4. Delete a Recipe")
+        print("5. Display all Recipes")
+        print("6. Exit")
+
+
+        choice = input("Choose an option (1-6): ")
+
+
+        if choice == "1":
+            recipe = input("Enter the name of the recipe: ")
+            create(recipe)
+        elif choice == "2":
+            index = input("Enter the index of the recipe to read: ")
+            index = int(index)
+            read(index)
+        elif choice == "3":
+            index = input("Enter the index of the recipe to update: ")
+            recipe = input("Enter the name of the recipe you want to update it with: ")
+            index = int(index)
+            update(index, recipe)
+        elif choice == "4":
+            index = input("Enter the index of the recipe to delete: ")
+            index = int(index)
+            destroy(index)
+        elif choice == "5":
+            list_all_recipes()
+        elif choice == "6":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+main()
