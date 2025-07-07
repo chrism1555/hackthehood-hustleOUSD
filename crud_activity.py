26ccf05d36005e7aeb5972f408289a6a66cd4f39
Christian Padilla <hazrdous.51010@gmail.com>
2025-07-06 23:11:17 -0700

    7/6

---
+++ crud_activity.py
@@ -0,0 +1,23 @@
+cookbook_list = []
+
+def create(recipe):
+
+      print("A recipe has been added to the list")
+
+      return
+
+def read(index):
+      
+      return index()
+
+def update(index, recipe):
+      
+      print("The recipe has been updated")
+
+      return
+
+def destroy():
+      
+      print("Statement that shows waht recipe has been removed")\
+      
+      return
